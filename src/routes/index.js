79b779d001c56417044f296238027d5bbbdf481f
Susana Santos Moreno <susanasantosmoreno@MacBook-Pro-de-Susana.local>
2020-07-01 16:06:59 +0200

    getting started with EJS using a template.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,7 @@ const router = express.Router();
 
 //routes
 router.get('/', (req, res) => {
-    res.render('index.html', {title: 'FIRST WEB SITE'});
-})
-
-router.get('/contact', (req, res) => {
-    res.render('contact.html', {title: 'CONTACT PAGE'});
+    res.render('index.html', {title: 'LOGO'});
 })
 
 module.exports = router;
